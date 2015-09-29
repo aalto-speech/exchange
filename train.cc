b50a305af266fedf2fc27d2f5c31c31586df5434
Matti Varjokallio <matti.varjokallio@iki.fi>
2015-09-29 12:44:16 +0300

    More content to train main.

--- train.cc
+++ train.cc
@@ -11,6 +11,14 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     try {
+        conf::Config config;
+        config("usage: train [OPTION...] CORPUS MODEL\n")
+          ('h', "help", "", "", "display help");
+        config.default_parse(argc, argv);
+        if (config.arguments.size() != 2) config.print_help(stderr, 1);
+
+        string corpus_fname = config.arguments[0];
+        string model_fname = config.arguments[1];
 
     } catch (string &e) {
         cerr << e << endl;

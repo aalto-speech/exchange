19cd4111e17796bd9eea1c426c8629706da2aa8a
Matti Varjokallio <matti.varjokallio@aalto.fi>
2015-09-30 10:17:07 +0300

    Data types and word count reading.

--- Exchange.cc
+++ Exchange.cc
@@ -1,5 +1,43 @@
+#include <sstream>
+
 #include "Exchange.hh"
+#include "io.hh"
 
 using namespace std;
 
 
+void
+Exchange::read_corpus(string fname) {
+
+    string line;
+
+    SimpleFileInput corpusf(fname);
+    set<string> word_types;
+    while (corpusf.getline(line)) {
+        stringstream ss(line);
+        string token;
+        while (ss >> token) word_types.insert(token);
+    }
+
+    for (auto wit=word_types.begin(); wit != word_types.end(); ++wit) {
+        m_vocabulary.push_back(*wit);
+        m_vocabulary_lookup[*wit] = m_vocabulary.size() - 1;
+    }
+    word_types.clear();
+
+    SimpleFileInput corpusf2(fname);
+    while (corpusf2.getline(line)) {
+        vector<int> sent;
+        stringstream ss(line);
+        string token;
+        while (ss >> token) sent.push_back(m_vocabulary_lookup[token]);
+
+        for (unsigned int i=0; i<sent.size(); i++)
+            m_word_counts[sent[i]]++;
+        for (unsigned int i=0; i<sent.size()-1; i++) {
+            m_word_bigram_counts[sent[i]][sent[i+1]]++;
+            m_word_rev_bigram_counts[sent[i+1]][sent[i]]++;
+        }
+    }
+
+}
--- Exchange.hh
+++ Exchange.hh
@@ -1,11 +1,39 @@
 #ifndef EXCHANGE
 #define EXCHANGE
 
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+
 
 class Exchange {
 public:
-    Exchange() { };
-    ~Exchange();
+    Exchange(int num_classes) : m_num_classes(num_classes) { };
+    Exchange(int num_classes, std::string fname) : m_num_classes(num_classes) { read_corpus(fname); };
+    ~Exchange() { };
+
+    void read_corpus(std::string fname);
+    double likelihood();
+
+private:
+
+    int m_num_classes;
+
+    std::vector<std::string> m_vocabulary;
+    std::map<std::string, int> m_vocabulary_lookup;
+
+    std::vector<std::set<int> > m_classes;
+    std::vector<int> m_word_classes;
+
+    std::vector<int> m_word_counts;
+    std::vector<std::map<int, int> > m_word_bigram_counts;
+    std::vector<std::map<int, int> > m_word_rev_bigram_counts;
+
+    std::vector<int> m_class_counts;
+    std::vector<std::map<int, int> > m_class_bigram_counts;
+    std::vector<std::map<int, int> > m_class_rev_bigram_counts;
 };
 
 
